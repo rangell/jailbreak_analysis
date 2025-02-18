d089266b73fddbe63e21c7518d662e84f4192faa
Rico Angell <r.angell@nyu.edu>
2025-02-18 12:36:20 -0500

    updating refusal ablation

--- compute_refusal_ablation.py
+++ compute_refusal_ablation.py
@@ -1,5 +1,7 @@
 import argparse
+import copy
 import os
+import gc
 import json
 import glob
 import numpy as np
@@ -13,6 +15,43 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 from utils import expand_shortcut_model_name
 
 
+def get_all_activations(args, model, tokenizer, dataset):
+    jailbreak_activations = []
+    original_activations = []
+    for example in tqdm(dataset):
+        # Compute jailbreak residual stream activations at specified location
+        instruction = tokenizer.apply_chat_template(
+            example[1]["jailbreak_prompt_convo"],
+            add_generation_prompt=True,
+            tokenize=False
+        )
+        inputs = tokenizer(instruction, return_tensors="pt").input_ids  
+        acts = []
+        with model.trace(inputs) as trace:
+            for l in range(model.config.num_hidden_layers):
+                acts.append(model.model.layers[l].output.save())
+        acts = torch.cat([a[0] for a in acts], dim=0)
+        #jailbreak_activations.append(acts[:, args.position, :].detach().cpu())
+        jailbreak_activations.append(acts[:, -5:, :].detach().cpu())
+
+        # Compare with original instruction
+        instruction = tokenizer.apply_chat_template(
+            [{"role": "user", "content": example[1]["original_prompt_text"]}],
+            add_generation_prompt=True,
+            tokenize=False
+        )
+        inputs = tokenizer(instruction, return_tensors="pt").input_ids  
+        acts = []
+        with model.trace(inputs) as trace:
+            for l in range(model.config.num_hidden_layers):
+                acts.append(model.model.layers[l].output.save())
+        acts = torch.cat([a[0] for a in acts], dim=0)
+        #original_activations.append(acts[:, args.position, :].detach().cpu())
+        original_activations.append(acts[:, -5:, :].detach().cpu())
+
+    return torch.stack(jailbreak_activations), torch.stack(original_activations)
+
+
 def get_activations(args, model, tokenizer, dataset):
     jailbreak_activations = []
     original_activations = []
@@ -23,12 +62,6 @@ def get_activations(args, model, tokenizer, dataset):
             add_generation_prompt=True,
             tokenize=False
         )
-        #inputs = tokenizer(instruction, return_tensors="pt").input_ids  
-        #acts = []
-        #with model.trace(inputs) as trace:
-        #    for l in range(model.config.num_hidden_layers):
-        #        acts.append(model.model.layers[l].output.save())
-        #acts = torch.cat([a[0] for a in acts], dim=0)
         inputs = tokenizer(instruction, return_tensors="pt").input_ids  
         with model.trace(inputs) as trace:
             acts = model.model.layers[args.layer].output.save()
@@ -43,18 +76,14 @@ def get_activations(args, model, tokenizer, dataset):
             tokenize=False
         )
         inputs = tokenizer(instruction, return_tensors="pt").input_ids  
-        #acts = []
-        #with model.trace(inputs) as trace:
-        #    for l in range(model.config.num_hidden_layers):
-        #        acts.append(model.model.layers[l].output.save())
-        #acts = torch.cat([a[0] for a in acts], dim=0)
-        #original_activations.append(acts[:, args.position, :].detach().cpu())
         with model.trace(inputs) as trace:
             acts = model.model.layers[args.layer].output.save()
         if isinstance(acts, tuple):
             acts = acts[0]
         original_activations.append(acts[:, args.position].detach().cpu())
-    #return torch.stack(jailbreak_activations), torch.stack(original_activations)
+
+        gc.collect()
+        torch.cuda.empty_cache()
     return torch.cat(jailbreak_activations, dim=0), torch.cat(original_activations, dim=0)
 
 
@@ -76,14 +105,21 @@ def augment_shard_with_refusal_ablation(args, model, tokenizer, refusal_directio
     # Get the activations at the best refusal direction location
     jailbreak_activations, original_activations = get_activations(args, model, tokenizer, shard_dataset)
 
-    from IPython import embed; embed(); exit()
+    # Compute refusal ablation
+    jailbreak_scores = jailbreak_activations @ refusal_direction
 
     # TODO: compute logits for next token prediction
     # TODO: record vector at position, project onto output space maybe quantize?
-    shard_data = {k: v for k, v in shard_dataset}
+    for idx, (k, v) in enumerate(shard_dataset):
+        _v = copy.deepcopy(v)
+        _v["refusal_score"] = jailbreak_scores[idx].item()
+        shard_data[k] = _v
 
     with open(shard_fname, "wb") as f:
         pickle.dump(shard_data, f)
+
+    gc.collect()
+    torch.cuda.empty_cache()
 
 
 def main(args):
@@ -101,9 +137,18 @@ def main(args):
     )
     model = NNsight(model)
 
-    # Grab the refusal direction
-    with open(f"{args.refusal_directions_dir}/{args.model_name}.pkl", "rb") as f:
-        refusal_direction_dict = pickle.load(f)
+    ## Grab the refusal direction
+    #with open(f"{args.refusal_directions_dir}/{args.model_name}.pkl", "rb") as f:
+    #    refusal_direction_dict = pickle.load(f)
+    assert args.model_name == "llama3-8b"
+    refusal_direction = torch.load("refusal_direction/pipeline/runs/meta-llama-3-8b-instruct/direction.pt").detach().cpu().to(torch.float32)
+    with open("refusal_direction/pipeline/runs/meta-llama-3-8b-instruct/direction_metadata.json", "r") as f:
+        refusal_location = json.load(f)
+    refusal_direction_dict = {
+        "best_refusal_direction": refusal_direction,
+        "best_position": refusal_location["pos"],
+        "best_layer": refusal_location["layer"]
+    }
 
     # Augment shards
     for shard_fname in tqdm(glob.glob(f"{args.responses_dir}/{args.model_name}*.pkl")):
